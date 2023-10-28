50c948f7b021516b6c0188d709fce75e0b2ad664
obogeanu2712 <obogeanumateirazvan@gmail.com>
2023-10-28 16:31:01 +0300

    segfaultproblem

--- main.cpp
+++ main.cpp
@@ -5,43 +5,53 @@
 #include <map>
 #include <utility>
 #include <iostream>
+#include <vector>
+
+using WaveformReader = capnp::List<int16_t, capnp::Kind::PRIMITIVE>::Reader;
+
 int main() {
 
-    std::map<std::pair<uint8_t, uint8_t>, uint16_t> channelBoardCounts;
+    std::map<std::pair<uint8_t, uint8_t>, WaveformReader> waveforms;
 
     // Open the .cap file for reading
-    kj::FdInputStream inputStream(open("Run_000001.cap", O_RDONLY));
+    int fd = open("Run_000001.cap", O_RDONLY);
+
+    if(fd == -1) {
+        std::cout << "File couldn't be opened" << std::endl;
+        return 1;
+    } 
+
+    kj::FdInputStream inputStream(fd);
     kj::BufferedInputStreamWrapper bufferedStream(inputStream);
     // Read the .cap file
     
     while (bufferedStream.tryGetReadBuffer() != nullptr) {
         
         capnp::PackedMessageReader message(bufferedStream, {100000000, 64});
-        Dataset::Reader dataset = message.getRoot<Dataset>();
+        WaveData::Reader wavedata = message.getRoot<WaveData>();
 
-        auto Events = dataset.getEvents();
+        auto waveEvents = wavedata.getEvents();
 
         // Iterate through the events
-        for (auto event : Events) {
+        for (auto event : waveEvents) {
             // // Access event fields
             uint8_t board = event.getBoard();
             uint8_t channel = event.getChannel();
-            channelBoardCounts[std::make_pair(board, channel)]++;
-            // // uint16_t energy = event.getEnergy();
-            // // uint64_t timestamp = event.getTimestamp();
-            // // float psd = event.getPsd();
-            // // auto waveform = event.getWaveform();
-
-            // // Process the event data as needed
-
-
+            // WaveformReader waveform1 = event.getWaveform1();
+            waveforms[std::make_pair(board, channel)] = event.getWaveform1();
         }
 
     }
-    for(const auto& object : channelBoardCounts) {
-        std::cout << "Board " << static_cast<int>(object.first.first) << ", Channel " 
-            << static_cast<int>(object.first.second)
-            << ": " << static_cast<int>(object.second) << " counts" << std::endl;
+
+    ///Print the stored values for the wave form
+    for (const auto waveform : waveforms) {
+        std::cout << "Board " << static_cast<int>(waveform.first.first) << " and Channel " 
+            << static_cast<int>(waveform.first.second) << ":" << std::endl;
+        auto waveform_values = waveform.second;
+        for(int i = 0; i < waveform_values.size(); i++) {
+            int value = static_cast<int>(waveform_values[i]);
+            std::cout << value << " ";
+        }
+        std::cout << std::endl;
     }
-    return 0;
 }
